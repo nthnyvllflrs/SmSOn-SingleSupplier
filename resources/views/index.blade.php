a6cae17ad2740dcdc4b62a87fa37bba816a245bc
nthnyvllflrs <anthonvillaflor@gmail.com>
2019-10-15 11:46:35 +0800

    ...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SmSOn</title>
+        <title>Laravel</title>
 
         <meta name="csrf-token" content="{{ csrf_token() }}" />
  
