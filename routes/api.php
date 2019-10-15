77246de3f304b7272cfebed5256e167ec0bce220
nthnyvllflrs <anthonvillaflor@gmail.com>
2019-10-15 11:38:56 +0800

    Added laravel-passport and AuthController with register, login, and logout function with respective api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['json.response']], function () {
+
+    // public routes
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+
+    // private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'AuthController@logout');
+
+        Route::get('/user', function (Request $request) {
+            return $request->user(); // Return current logged in user
+        });
+    });
 });
